da46ca4fb3a7a55b097ef4369d851d4bb7142de1
vineetdhimancode <vineetdhiman1995@gmail.com>
2020-09-19 12:54:11 +0530

    created character counter

---
+++ characterCounter/js/app.js
@@ -0,0 +1,35 @@
+const str = document.querySelector('#string');
+const result = document.querySelector('.result');
+str.addEventListener('paste', eventFunction);
+str.addEventListener('keypress', eventFunction);
+str.addEventListener('keyup', eventFunction);
+str.addEventListener('cut', eventFunction);
+str.addEventListener('change', eventFunction);
+
+
+function eventFunction() {
+    // alert(e.target.value);
+    let strValue = '';
+    setTimeout(function() {
+        strValue = str.value.trim();
+        let charCount = strValue.length;
+        showMsg(charCount);
+    }, 1);
+}
+
+
+function showMsg(charCount) {
+    if (charCount >= 120) {
+        result.classList.add('text-danger');
+        result.classList.remove('text-success');
+        result.innerHTML = `Hey Prabhu! ${charCount} characters, You should fix it!`;
+    } else if (charCount <= 0) {
+        result.classList.add('text-danger');
+        result.classList.remove('text-success');
+        result.innerHTML = `No character captured!`;
+    } else {
+        result.classList.add('text-success');
+        result.classList.remove('text-danger');
+        result.innerHTML = `Wallah! ${charCount} characters, You did a great job!`;
+    }
+}
